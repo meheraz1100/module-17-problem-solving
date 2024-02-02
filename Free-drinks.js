ac2526853226b4bdb6612694bb5ec3d2d1987996
meheraz1100 <meheraz1100@gmail.com>
2024-02-02 08:10:16 +0600

    First problem solved

---
+++ Free-drinks.js
@@ -0,0 +1,10 @@
+let burgerPrice = 500;
+let cokePrice;
+
+if(burgerPrice >= 500){
+    console.log('You get a free coke');
+}
+else{
+    let message = 'Coke price 30 tk because your burger price is less then 500 tk';
+    console.log(message);
+}
